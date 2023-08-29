ef1a81e260840e8ec33584ca7c58c4cde79fa872
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-08-30 02:38:00 +0530

    fixed env variables

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import FacebookProvider from "next-auth/providers/facebook";
 export const authOptions: NextAuthOptions = {
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET as string,
     }),
   ],
 };
