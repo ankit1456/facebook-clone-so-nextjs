294bdbbb5a706523abcf8a70d3a76fdfaa11539d
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-09-02 14:48:26 +0530

    fixed post styling

--- components/Post.tsx
+++ components/Post.tsx
@@ -11,7 +11,7 @@ type PostProps = {
 };
 const Post = ({ post }: PostProps) => {
   return (
-    <div className="bg-white flex flex-col rounded-xl border bottom-1 border-gray-200 shadow-sm">
+    <div className="bg-white flex flex-col rounded-xl border bottom-1 border-gray-200 shadow-sm mt-5">
       <div className="p-4">
         <div className="flex items-center space-x-2">
           <Image
@@ -36,16 +36,17 @@ const Post = ({ post }: PostProps) => {
         <p className="p-4 py-3 -mb-2">{post.data().message}</p>
       </div>
 
-      <div className="relative h-56 md:h-96 bg-white">
-        <Image
-          src={post.data().postImage}
-          alt="post image"
-          objectFit="cover"
-          layout="fill"
-        />
-      </div>
-
-      <div className="bg-white flex justify-between items-centertext-gray-400 rounded-b-xl">
+      {post.data().postImage && (
+        <div className="relative h-56 md:h-96 bg-white">
+          <Image
+            src={post.data().postImage}
+            alt="post image"
+            objectFit="cover"
+            layout="fill"
+          />
+        </div>
+      )}
+      <div className="bg-white flex justify-between items-centertext-gray-400 rounded-b-xl border-t">
         <div className="inputIcon space-x-2">
           <HandThumbUpIcon className="h-4" />
           <p className="text-xs sm:text-base">Like</p>
